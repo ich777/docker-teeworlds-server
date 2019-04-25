11b1abe9c1283648868a3c54478b2d91dc493176
ich777 <christophhummer@gmail.com>
2019-04-25 17:23:35 +0200

    Update start-server.sh

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+sleep infinity
+
+if [ ! -f ${SERVER_DIR}/teeworlds_srv ]; then
+    echo "Teeworlds not found!"
+    wget -q -O ${STEAMCMD_DIR}/steamcmd_linux.tar.gz http://media.steampowered.com/client/steamcmd_linux.tar.gz 
+    tar --directory ${STEAMCMD_DIR} -xvzf /serverdata/steamcmd/steamcmd_linux.tar.gz
+    rm ${STEAMCMD_DIR}/steamcmd_linux.tar.gz
+fi
+
 echo "---Install Server---"
 sleep infinity
