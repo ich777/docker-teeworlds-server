c0702728012554d4211dbae7da12744b462f8726
ich777 <christophhummer@gmail.com>
2019-04-25 16:51:27 +0200

    Update start-server.sh

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,11 +1,2 @@
 echo "---Install Server---"
 sleep infinity
-<TODO>
-
-
-echo "---Prepare Server---"
-chmod -R 770 ${DATA_DIR}
-
-echo "---Start Server---"
-cd ${SERVER_DIR}
-./teeworlds_srv -f ${CONFIG_DIR}/${CONFIG_FILE}
